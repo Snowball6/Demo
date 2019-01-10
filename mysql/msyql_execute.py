c4ec7cc0877c172f327931f9de02b81d3ff52dad
Chenzhipeng <875202690@qq.com>
2019-01-10 09:27:22 +0800

    mysql查询demo

--- mysql/msyql_execute.py
+++ mysql/msyql_execute.py
@@ -2,6 +2,11 @@
 # Author:ChenZhipeng
 # Time:2019/1/10 9:15
 
+# Python查询Mysql使用 fetchone() 方法获取单条数据, 使用fetchall() 方法获取多条数据。
+# 
+# fetchone(): 该方法获取下一个查询结果集。结果集是一个对象
+# fetchall(): 接收全部的返回结果行.
+# rowcount: 这是一个只读属性，并返回执行execute()方法后影响的行数。
 import pymysql
 conn = pymysql.connect(host="localhost", user="root", password="680034", db="test", port=3306)
 cur = conn.cursor()
