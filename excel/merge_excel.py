d30927c13d42aacbd57e3a7feb23187e6223e237
Chenzhipeng <875202690@qq.com>
2019-01-11 18:43:48 +0800

    .

--- excel/merge_excel.py
+++ excel/merge_excel.py
@@ -1,12 +1,19 @@
 # -*- coding:utf-8 -*-
 # Author:ChenZhipeng
 # Time:2019/1/11 14:53
-# https://blog.csdn.net/fanlei_lianjia/article/details/78225857
+
+# https://openpyxl.readthedocs.io/en/stable/tutorial.html
+
 import openpyxl
-wb_data = openpyxl.load_workbook("merge_test.xlsx")  # 数据表
-wb_merge = openpyxl.load_workbook("merge.xlsx")    # 最终表
-ws = wb_merge.get_sheet_by_name("merge")  # 操作最终表中的merge这个sheet表
-op_sheet = wb_data.sheetnames
-for cloumn_name in op_sheet:
-    print(cloumn_name)
-    
+wb_total = openpyxl.Workbook("Jan_upper.xlsx")  # 新建工作薄
+wb_data = openpyxl.load_workbook('data.xlsx')  # 打开需要分析的工作薄
+ws = wb_total.create_sheet("merge")         # 新建sheet
+ws.sheet_properties.tabColor = "1072BA"     # 给sheet表添加颜色
+op_sheet = wb_data.sheetnames   # 获取分析的工作薄中的sheet名
+for sheet in wb_data:
+    print(sheet.title)
+
+
+
+
+#wb_total.save("Jan_upper.xlsx")
