2b03780914b2a39e39d9e01d5a05fa09b4b96707
Chenzhipeng <875202690@qq.com>
2019-02-13 14:50:03 +0800

    .

--- test.py
+++ test.py
@@ -63,34 +63,68 @@
 # tom = Cat("tom")
 # print(tom)
 
-class Person:
-    def __init__(self, name, height):
+# class Person:
+#     def __init__(self, name, height):
+#         self.name = name
+#         self.height = height
+#
+#     def __str__(self):
+#         return "我的名字叫%s, 我的身高是%s" % (self.name, self.height)
+#
+#     def run(self):
+#         print("%s " % self.name)
+#         self.height -= 0.5
+#
+#     def eat(self):
+#         print("%s " % self.name)
+#         self.height += 1
+#
+#
+# people = Person("xiaoming", 180)
+# people.run()
+# print(people)
+
+
+class HouseItem(object):
+
+    def __init__(self, name, area):
         self.name = name
-        self.height = height
+        self.area = area
 
     def __str__(self):
-        return "我的名字叫%s, 我的身高是%s" % (self.name, self.height)
-
-    def run(self):
-        print("%s " % self.name)
-        self.height -= 0.5
-
-    def eat(self):
-        print("%s " % self.name)
-        self.height += 1
+        return "add furniture %s, area is %.2f" % (self.name, self.area)
 
 
-people = Person("xiaoming", 180)
-people.run()
-print(people)
+bed = HouseItem("席梦思", 4)
+table = HouseItem("书桌", 3)
+chest = HouseItem("书柜", 1)
 
 
+class House(object):
+
+    def __init__(self, house_type, area):
+        self.house_type = house_type
+        self.total_area = area
+        # 剩余面积
+        self.free_area = area
+        # 家具列表
+        self.furniture_list = []
+
+    def add_item(self, item):
+        if item.area > self.total_area:
+            print("so big")
+            return
+        else:
+            self.free_area -= item.area
+            self.furniture_list.append(item.name)
+
+    def __str__(self):
+        return "房子的户型为%s,\n总面积：%.2f\n剩余面积：%.2f\n共有家具为%s" \
+               % (self.house_type, self.total_area, self.free_area, self.furniture_list)
 
 
-
-
-
-
-
-
-
+my_home = House("2居", 100)
+my_home.add_item(bed)
+my_home.add_item(table)
+my_home.add_item(chest)
+print(my_home)
