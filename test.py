34368dadd8259c419345758b028681c6a3810405
Chenzhipeng <875202690@qq.com>
2019-02-14 10:11:26 +0800

    .

--- test.py
+++ test.py
@@ -85,46 +85,76 @@
 # print(people)
 
 
-class HouseItem(object):
+# class HouseItem(object):
+#
+#     def __init__(self, name, area):
+#         self.name = name
+#         self.area = area
+#
+#     def __str__(self):
+#         return "add furniture %s, area is %.2f" % (self.name, self.area)
+#
+#
+# bed = HouseItem("席梦思", 4)
+# table = HouseItem("书桌", 3)
+# chest = HouseItem("书柜", 1)
+#
+#
+# class House(object):
+#
+#     def __init__(self, house_type, area):
+#         self.house_type = house_type
+#         self.total_area = area
+#         # 剩余面积
+#         self.free_area = area
+#         # 家具列表
+#         self.furniture_list = []
+#
+#     def add_item(self, item):
+#         if item.area > self.total_area:
+#             print("so big")
+#             return
+#         else:
+#             self.free_area -= item.area
+#             self.furniture_list.append(item.name)
+#
+#     def __str__(self):
+#         return "房子的户型为%s,\n总面积：%.2f\n剩余面积：%.2f\n共有家具为%s" \
+#                % (self.house_type, self.total_area, self.free_area, self.furniture_list)
+#
+#
+# my_home = House("2居", 100)
+# my_home.add_item(bed)
+# my_home.add_item(table)
+# my_home.add_item(chest)
+# print(my_home)
 
-    def __init__(self, name, area):
+
+class Women:
+
+    def __init__(self, name):
         self.name = name
-        self.area = area
+        self.__age = 18
 
-    def __str__(self):
-        return "add furniture %s, area is %.2f" % (self.name, self.area)
-
-
-bed = HouseItem("席梦思", 4)
-table = HouseItem("书桌", 3)
-chest = HouseItem("书柜", 1)
+    def secret(self):
+        print("%s 的年龄是 %s" % (self.name, self.__age))
 
 
-class House(object):
-
-    def __init__(self, house_type, area):
-        self.house_type = house_type
-        self.total_area = area
-        # 剩余面积
-        self.free_area = area
-        # 家具列表
-        self.furniture_list = []
-
-    def add_item(self, item):
-        if item.area > self.total_area:
-            print("so big")
-            return
-        else:
-            self.free_area -= item.area
-            self.furniture_list.append(item.name)
-
-    def __str__(self):
-        return "房子的户型为%s,\n总面积：%.2f\n剩余面积：%.2f\n共有家具为%s" \
-               % (self.house_type, self.total_area, self.free_area, self.furniture_list)
+monika = Women("wonika")
+print(monika._Women__age)
+monika._Women__secret()
 
 
-my_home = House("2居", 100)
-my_home.add_item(bed)
-my_home.add_item(table)
-my_home.add_item(chest)
-print(my_home)
+
+
+
+
+
+
+
+
+
+
+
+
+
