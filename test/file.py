3af0b3b9e5f096f56b1d048d730f29aaf828ca76
Chenzhipeng <875202690@qq.com>
2019-02-22 16:05:13 +0800

    .

---
+++ test/file.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# Author:ChenZhipeng
+# Time:2019/2/22 15:52
+
+
+# 复制小文件
+# with open("读取.txt", "r") as f:
+#     content = f.read()
+#
+# with open("写入.txt", "w") as w:
+#     w.write(content)
+
+# 复制大文件
+
+with open("读取.txt", "r+") as f:
+    while True:
+        content = f.readline()
+        if not content:
+            break
+        with open("写入.txt", "a") as w:
+            w.write(content)
+
