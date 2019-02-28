2929a50fa709fbc9d179015061ce39b533cab358
Chenzhipeng <875202690@qq.com>
2019-02-28 16:10:21 +0800

    redis 自动化安装脚本

---
+++ middleware/niginx_install.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+# Author:ChenZhipeng
+# Time:2019/2/27 17:15
---
+++ middleware/redis_install.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+# Author:ChenZhipeng
+# Time:2019/2/28 14:16
+
+import subprocess
+import os
+
+
+def redis_install():
+    """
+    1 、 install_path : 软件包（tar、rpm）包存放路径，请手动新建一个目录，并将tar包放到该路径下,一定要以 / 结尾
+    2 、 redis_home_path :解压包后 redis的部署路径，一定要以  / 结尾
+    3、  configuration_dir : redis.conf配置文件存放路径，建议在 部署路径中建立/etc文件存储 ,一定要以  / 结尾
+    :return:
+    """
+    install_path = "/tmp/redis/"
+    redis_dir = "/usr/local/redis/"
+    configuration_dir = "/usr/local/redis/etc/"
+    if not os.path.exists(redis_dir,):
+        os.makedirs(redis_dir,)
+    if not os.path.exists(configuration_dir):
+        os.makedirs(configuration_dir)
+    tar_name = subprocess.Popen("cd %s && ls" % install_path, shell=True, stdout=subprocess.PIPE).stdout.readline().strip().decode()
+    subprocess.Popen("cd %s && tar -zxvf %s" % (install_path, tar_name), shell=True).wait()
+    redis_name = subprocess.Popen("cd %s && ls" % install_path, shell=True, stdout=subprocess.PIPE).stdout.readline().strip().decode()
+    subprocess.Popen("cd %s%s && make" % (install_path, redis_name), shell=True,).wait()
+    subprocess.Popen("cd %s%s/src && make install PREFIX=%s" % (install_path, redis_name, redis_dir), shell=True,).wait()
+
+    subprocess.Popen("cd %s%s && mv redis.conf %s" %(install_path, redis_name, configuration_dir), shell=True).wait()
+    os.system("sed -i 's/daemonize no/daemonize yes/g' %sredis.conf" % configuration_dir)
+    subprocess.Popen("echo %sbin/redis-server >> /etc/rc.local && echo %sredis.conf >> /etc/rc.local" % (redis_dir, configuration_dir), shell=True)
+    subprocess.Popen("%sbin/redis-server %sredis.conf >> /etc/rc.local" % (redis_dir, configuration_dir), shell=True)
+    print("----------%s 已经成功安装----------" % redis_name)
+
+
+def main():
+    redis_install()
+
+
+if __name__ == "__main__":
+    main()
+
+
