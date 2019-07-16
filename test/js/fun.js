2cb3d769f29940b376d19c0faa04b7cb0304f30e
Chenzhipeng <875202690@qq.com>
2019-07-16 17:12:22 +0800

    。

---
+++ test/js/fun.js
@@ -0,0 +1,24 @@
+// function f(a,b) {
+//     return a+b;
+// }
+// console.log(f(1,3));
+
+// var a1 = [11,22,33,44];
+// a1.forEach(function (a) {
+//    console.log(a)
+// });
+
+// function sum(a,b) {
+//     var ret = 0;
+//     for(var i=0;i<arguments.length;i++){
+//         ret += arguments[i]
+//     }
+//     return ret;
+// }
+// console.log(sum(10,20,30));
+
+var s2 = 100;
+function f() {
+    console.log(s2)
+}
+f()
