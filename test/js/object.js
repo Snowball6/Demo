31d714d597a4fa1c8a1155583c79c1ea1d9cbd82
Chenzhipeng <875202690@qq.com>
2019-07-17 17:17:39 +0800

    。

---
+++ test/js/object.js
@@ -0,0 +1,13 @@
+// var o1 = {"a":100, "b":200};
+// for (var i in o1){
+//     console.log(i);
+//     console.log(o1[i])
+// }
+
+// var today = new Date();
+// console.log(today);   //Wed Jul 17 2019 09:35:08 GMT+0800 (中国标准时间)
+// console.log(today.toLocaleString());   //2019/7/17 上午9:39:06
+// console.log(today.getDay());
+
+var phone = new RegExp("^1[3-9][0-9]{9}$");
+phone.test("130012433333");
